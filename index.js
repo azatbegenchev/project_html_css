efe6f5e49e8809841922f5e21d471dc3d431d0eb
Azat <azat.begenchev@yandex.ru>
2021-11-24 23:07:12 +0500

    first_commit

---
+++ index.js
@@ -0,0 +1,95 @@
+/* variables
+var myMessage;
+myMessage = "msg";
+
+var myNumber = 10.484;
+var myString = "Hello";
+var myBoolen = true;
+var myNull = null;
+var myUndefinded = undefined;
+var myObject = {};
+let a = 125;
+let b = 36;
+
+
+console.log(a + b, myString)*/
+
+ //conditions object car
+
+
+/*
+if (car.color == 'green') {
+
+    console.log('The car is green');
+    
+} else if(car.color == 'red')  {
+
+    console.log('The car is red');
+    
+} else{
+    console.log('The color is undifined')
+}*/
+
+/*if (car.cost <= 2400) {
+    
+    console.log('The car is to expensive')
+
+} else {
+    console.log('The car is not to expensive')
+}
+*/
+// let car = {
+
+//     color:"red",
+//     cost: 2300,
+//     maxSpeed: 240,
+
+// };
+
+
+// let car2 = {
+//     color: 'green',
+// };
+
+
+
+// colorChange(car2);
+
+
+
+
+// function colorChange(theCar) {
+//     theCar.color = 'yellow';
+// }
+
+// console.log("Car: " + car.color);
+// console.log("Car2: " + car2.color)
+
+
+
+// function speedChange(Thespeed) {
+//     console.log("Скорость была:" + car.maxSpeed);
+//     Thespeed.maxSpeed = 300;
+//     console.log("Скорость стала:" + Thespeed.maxSpeed); 
+// }
+
+// speedChange(car);
+
+// let myName = window.prompt('What is your name');
+
+
+// console.log('Welcome to JS, Mr ' + myName);
+
+let color = getColor;
+color();
+
+ function getColor(color = "red") {
+	if (color == "red") {
+		return;
+	}
+
+	console.log(color);
+};
+
+  
+
